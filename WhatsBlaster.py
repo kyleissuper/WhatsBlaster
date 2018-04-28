34321c7994fe2cb8dc5c7980488fd45d4d296674
Kyle Tan <kyle@kyletan.com>
2018-04-28 15:14:53 +0800

    Updated to fix element cant be found error

--- WhatsBlaster.py
+++ WhatsBlaster.py
@@ -20,7 +20,7 @@ class WhatsBlaster:
     def send_message(self, contact_name, message, timeout=10):
         driver = self.driver
         # Find contact
-        search = driver.find_element_by_css_selector("input.input-search")
+        search = driver.find_element_by_css_selector("input[title='Search or start new chat']")
         search.send_keys(contact_name)
         try:
             element = WebDriverWait(driver, 10).until(
@@ -30,25 +30,24 @@ class WhatsBlaster:
         except:
             return "Failure: could not find contact {}.".format(contact_name)
         # Load chat
-        time.sleep(2)
+        time.sleep(4)
         driver.find_element_by_css_selector(
-                "span[title='{}']".format(contact_name)
+                "#pane-side span[title='{}']".format(contact_name)
                 ).click()
         try:
             element = WebDriverWait(driver, 10).until(
                     lambda driver: driver.find_element_by_css_selector(
-                        ".pane-chat [title='{}']".format(contact_name)
+                        "#main [title='{}']".format(contact_name)
                         ))
         except:
             return "Failure: could not load chat with contact {}.".format(contact_name)
-        # Send message
-        input_element = driver.find_element_by_css_selector("div.input")
+        # Send message + wait until successful
+        messages_0 = driver.find_elements_by_css_selector("#main span.selectable-text.invisible-space.copyable-text")
+        input_element = driver.find_element_by_css_selector("footer div.copyable-text.selectable-text")
         input_element.send_keys(message)
         input_element.send_keys(Keys.ENTER)
-        # Wait until successful
-        messages_0 = driver.find_elements_by_css_selector(".message-list .msg")
         for t in range(timeout):
-            messages_1 = driver.find_elements_by_css_selector(".message-list .msg")
+            messages_1 = driver.find_elements_by_css_selector("#main span.selectable-text.invisible-space.copyable-text")
             if len(messages_0) != len(messages_1):
                 return "Success: sent to {}.".format(contact_name)
             time.sleep(1)
